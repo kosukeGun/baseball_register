258e83208cd2950602543ea28270255aed2ef3d8
kosukeGun <ko.iwai09210921@icloud.com>
2023-06-29 16:33:00 +0900

    【fix】カードに詳細画面遷移ボタンを実装

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -6,14 +6,15 @@
 @foreach($games as $game)
 <div class="col-sm-3">
     <div class="card bg-light" style="text-align:center;">
-        <div class="card-img-top">
-            <h2>{{$game->tournament->name}}</h2>
-        </div>
-        <div class="card-body">
-            <h3 class="card-title">{{$game->name}}</h5>
-            <p class="card-text">{{$game->date}}</p>
-            <a href="/games/{{$game->id}}" class="btn btn-primary">詳細</a>
-        </div>
+        <button onclick="location.href='/games/{{$game->id}}'" class="btn btn-primary">
+            <div class="card-img-top">
+                <h2>{{$game->tournament->name}}</h2>
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">{{$game->name}}</h5>
+                <p class="card-text">{{$game->date}}</p>
+            </div>
+        </button>
     </div>
 </div>
 @endforeach
--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -6,14 +6,15 @@
 @foreach($members as $member)
 <div class="col-sm-3">
     <div class="card bg-light" style="text-align:center;">
-        <div class="card-img-top">
-            <h2>{{$member->name_first}} {{$member->name_last}}</h2>
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">{{$member->grade->name}}</h5>
-            <p class="card-text">{{$member->type->name}}</p>
-            <a href="/datas/{{$member->id}}" class="btn btn-primary">詳細</a>
-        </div>
+        <button onclick="location.href='/datas/{{$member->id}}'" class="btn btn-primary">
+            <div class="card-img-top">
+                <h2>{{$member->name_first}} {{$member->name_last}}</h2>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">{{$member->grade->name}}</h5>
+                <p class="card-text">{{$member->type->name}}</p>
+            </div>
+        </button>
     </div>
 </div>
 @endforeach
