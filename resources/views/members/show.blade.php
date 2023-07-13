a6e8a99ea4ab003aa9f190bc88f1533ea00b3248
kosukeGun <ko.iwai09210921@icloud.com>
2023-07-13 11:14:22 +0900

    【update】成績表示のカード化

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -11,29 +11,53 @@
 <div class="type-group" style="text-align:center;">
     <h4>{{$member->type->name}}</h4>
 </div>
-<div class="hit-ratio-group" style="text-align:center;">
-    <label for="hit-ratio">打率</label>
-    @if(count($results[0]) + count($results[1]) + count($results[3]) + count($results[4]) != 0)
-    {{ sprintf('%.3f',count($results[0])/(count($results[0]) + count($results[1]) + count($results[3]) + count($results[4]))) }}
-    @else
-    ---
-    @endif
-</div>
-<div class="strikeout-group" style="text-align:center;">
-    <label for="strikeout">三振数</label>
-    {{ count($results[4]) }}
-</div>
-<div class="fdb-group" style="text-align:center;">
-    <label for="fdb">四死球数</label>
-    {{ count($results[2]) }}
-</div>
-<div class="homerun-group" style="text-align:center;">
-    <label for="homerun">ホームラン数</label>
-    {{ count($result_HR) }}
-</div>
-<div class="rbi-group" style="text-align:center;">
-    <label for="rbi">打点</label>
-    {{ $rbi }}
+<div class="row">
+    <div class="col-sm-4">
+        <div class="card text-center">
+            <div class="card-header">打率</div>   
+            <div class="card-body">
+                <p class="card-text">
+                @if(count($results[0]) + count($results[1]) + count($results[3]) + count($results[4]) != 0)
+                {{ sprintf('%.3f',count($results[0])/(count($results[0]) + count($results[1]) + count($results[3]) + count($results[4]))) }}
+                @else
+                ---
+                @endif
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="card text-center">
+            <div class="card-header">三振数</div>
+            <div class="card-body">
+                <p class="card-text">{{ count($results[4]) }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="card text-center">
+            <div class="card-header">四死球数</div>
+            <div class="card-body">
+                <p class="card-text">{{ count($results[2]) }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="card text-center">
+            <div class="card-header">ホームラン数</div>
+            <div class="card-body">
+                <p class="card-text">{{ count($result_HR) }}</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="card text-center">
+            <div class="card-header">打点</div>
+            <div class="card-body">
+                <p class="card-text">{{ $rbi }}</p>
+            </div>
+        </div>
+    </div>
 </div>
 <div class="edit-button" style="text-align:center;">
     <a type="button" class="btn btn-info" href="/datas/{{$member->id}}/edit">編集</a>
